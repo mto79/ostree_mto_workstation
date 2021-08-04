1806f21eeb1a4a15600a4e9335257668741f711c
Marc Ouwerkerk <marc@mto.nu>
2021-08-04 23:13:59 +0200

    compose.sh changed

--- compose.sh
+++ compose.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -eu
-CACHE=/var/cache/ostree
-REPO=/var/tmp/repo
+
+CACHE=/var/home/mto/ostree_mto_workstation/cache/ostree
+REPO=/var/home/mto/ostree_mto_workstation/repo
 
 mkdir -p $CACHE
+mkdir -p $REPO
+
+rm -Rf $CACHE/*
+rm -Rf $REPO/*
 
 if [ ! -d $REPO/objects ]; then
     ostree --repo=$REPO init --mode=archive-z2
 fi
 
 rpm-ostree compose tree --unified-core --cachedir=$CACHE --repo=$REPO mto-desktop.yaml
+
