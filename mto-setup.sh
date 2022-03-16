54ac7d213be3188dfdec94f11c18edd576fa9c77
mto <marc@mto.nu>
2022-03-16 10:31:46 +0100

    ifuse for iphone and ref to shotwell in setup

--- mto-setup.sh
+++ mto-setup.sh
@@ -50,7 +50,8 @@ flatpak install flathub org.jitsi.jitsi-meet -y
 flatpak install flathub com.mattermost.Desktop -y
 flatpak install flathub com.belmoussaoui.Authenticator -y
 flatpak install flathub org.libreoffice.LibreOffice -y 
-  
+flatpak install flathub org.gnome.Shotwell -y
+
 echo "Libvirt: Prevenpassword prompts for each and every libvirt action, install the following polkit rule."
 echo "-----------------------"
 
